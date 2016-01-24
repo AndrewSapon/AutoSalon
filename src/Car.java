ee86a1a2138821dad1aa7b327473542868b88523
Andrew <sapon_a@mail.ru>
2016-01-24 19:53:32 +0200

    Change Car

--- src/Car.java
+++ src/Car.java
@@ -2,4 +2,9 @@
  * Created by Andrew on 24.01.2016.
  */
 public class Car {
+    public static void main (String ... args)
+    {
+        String s = "Hallo World";
+        System.out.println(s);
+    }
 }
