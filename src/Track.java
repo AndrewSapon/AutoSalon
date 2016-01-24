f92dbea990c9572d3e41c784c4e994f62aecf035
Andrew <sapon_a@mail.ru>
2016-01-24 20:35:10 +0200

    finish class Track

--- src/Track.java
+++ src/Track.java
@@ -1,50 +1,22 @@
 
 public class Track {
-    /**
-     * Created by Andrew on 17.01.2016.
-     */
+
         private  Car car = null;
         private String number;
         private int status = 0;
-        // 0 - стою  1 - качусь
+        // статус прицепа: 0 - стою  1 - качусь
 
-        public Car getCar()
-        {
-            return car;
-        }
-
-        public void setCar(Car car)
-        {
-            this.car = car;
-        }
+        public Car getCar() {return car;}
+        public void setCar(Car car) {this.car = car;}
 
         public Track() {}
-        public Track(String number)
-        {
-            this.number = number;
-        }
+        public Track(String number) {this.number = number;}
         public Track(int status, String number) {this.status = status;this.number = number;}
 
-
-        public String getNumber()
-        {
-            return number;
-        }
-        public int getStatus()
-        {
-            return status;
-        }
-
-
-        public void setNumber(String number)
-        {
-            this.number = number;
-        }
-        public void setStatus(int status)
-        {
-            this.status = status;
-        }
-
+        public String getNumber() {return number;}
+        public int getStatus() {return status;}
+        public void setNumber(String number) {this.number = number;}
+        public void setStatus(int status) {this.status = status;}
 
         public void stop()
         {
